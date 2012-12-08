09b4edeca3cd6ebbfa00759ef179b86644e52ee1
Puneet Goyal <pungoyal@gmail.com>
2012-12-08 16:32:12 +0530

    first commit

---
+++ Gemfile
@@ -0,0 +1,12 @@
+source 'https://rubygems.org'
+
+gem 'rails'
+gem 'jquery-rails'
+gem 'mongoid'
+
+group :assets do
+  gem 'sass-rails',   '~> 3.2.3'
+  gem 'coffee-rails', '~> 3.2.1'
+
+  gem 'uglifier', '>= 1.0.3'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Railsgirls::Application.config.secret_token = '2dd22b0a87b33176ea14aa31c965997d26d8c40ae1e27fdf9b4c68f49973b18cffdfe370c000aa956f15f3dce0d3435e79381fe95476692bf0bdb6593cf87ed2'
